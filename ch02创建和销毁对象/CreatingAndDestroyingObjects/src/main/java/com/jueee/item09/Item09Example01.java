ca0267cf95ca845f1b76d1de3cf4cde36d93d7aa
尉勇强2 <hzweiyongqiang@corp.netease.com>
2019-09-24 15:34:52 +0800

    使用try-with-resources语句替代try-finally语句

---
+++ ch02创建和销毁对象/CreatingAndDestroyingObjects/src/main/java/com/jueee/item09/Item09Example01.java
@@ -0,0 +1,70 @@
+package com.jueee.item09;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class Item09Example01 {
+
+    private static final int BUFFER_SIZE = 10000;
+
+    // try-finally - 不再是关闭资源的最佳方法！
+    static String firstLineOfFile(String path) throws IOException {
+        BufferedReader br = new BufferedReader(new FileReader(path));
+        try {
+            return br.readLine();
+        } finally {
+            br.close();
+        }
+    }
+
+    // try-finally 当添加第二个资源时，情况会变得更糟
+    static void copy(String src, String dst) throws IOException {
+        InputStream in = new FileInputStream(src);
+        try {
+            OutputStream out = new FileOutputStream(dst);
+            try {
+                byte[] buf = new byte[BUFFER_SIZE];
+                int n;
+                while ((n = in.read(buf)) >= 0)
+                    out.write(buf, 0, n);
+            } finally {
+                out.close();
+            }
+        } finally {
+            in.close();
+        }
+    }
+
+    // try-with-resources - 关闭资源的最佳方法！
+    static String firstLineOfFileGood(String path) throws IOException {
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+            return br.readLine();
+        }
+    }
+
+    // try-with-resources on multiple resources - 关闭资源的最佳方法！
+    static void copyGood(String src, String dst) throws IOException {
+        try (InputStream in = new FileInputStream(src); OutputStream out = new FileOutputStream(dst)) {
+            byte[] buf = new byte[BUFFER_SIZE];
+            int n;
+            while ((n = in.read(buf)) >= 0)
+                out.write(buf, 0, n);
+        }
+    }
+
+    // try-with-resources with a catch clause
+    static String firstLineOfFile(String path, String defaultVal) {
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+            return br.readLine();
+        } catch (IOException e) {
+            return defaultVal;
+        }
+    }
+    
+    
+}
