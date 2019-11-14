69043c55bca1ff32feb56c804e1877439a38bc70
尉勇强2 <hzweiyongqiang@corp.netease.com>
2019-11-14 20:15:19 +0800

    接口仅用来定义类型

---
+++ ch04类和接口/ClassesAndInterfaces/src/main/java/com/jueee/item22/Item22Example01.java
@@ -0,0 +1,14 @@
+package com.jueee.item22;
+
+// 常量接口类 - 禁止这样使用！
+public interface Item22Example01 {
+    
+    // Avogadro's number (1/mol)
+    static final double AVOGADROS_NUMBER = 6.022_140_857e23;
+
+    // Boltzmann constant (J/K)
+    static final double BOLTZMANN_CONSTANT = 1.380_648_52e-23;
+
+    // Mass of the electron (kg)
+    static final double ELECTRON_MASS = 9.109_383_56e-31;
+}
---
+++ ch04类和接口/ClassesAndInterfaces/src/main/java/com/jueee/item22/Item22Example02.java
@@ -0,0 +1,10 @@
+package com.jueee.item22;
+
+public class Item22Example02 {
+    
+    private Item22Example02() { }  // 防止实例化
+
+    public static final double AVOGADROS_NUMBER = 6.022_140_857e23;
+    public static final double BOLTZMANN_CONST  = 1.380_648_52e-23;
+    public static final double ELECTRON_MASS    = 9.109_383_56e-31;
+}
